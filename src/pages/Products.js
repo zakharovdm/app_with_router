fb620442a599474d3600df7ebc0503fc29654491
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-11 23:07:10 +0300

    feat: added a transition to product details with __switch__

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section>
       <h1>The products page</h1>
       <ul>
-        <li>A book</li>
-        <li>A carpet</li>
-        <li>An Online Course</li>
+        <li>
+          <Link to="/products/p1">A book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">An Online Course</Link>
+        </li>
       </ul>
     </section>
   );
