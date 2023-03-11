9ba63854df7fc5b7b9c90a6f2b0b497ad43ebd50
Dmitriy Zakharov <dzaharov794@gmail.com>
2023-03-11 22:19:03 +0300

    extracting route params

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,7 +1,12 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  const param = useParams();
+
   return (
     <section>
       <h1>Product Detail</h1>
+      <p>{param.productID}</p>
     </section>
   );
 };
